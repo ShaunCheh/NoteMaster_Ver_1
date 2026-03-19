c9d6ee8a73b9229db198d3d21f0ebda49200b36b
xiangjie yu <shaunyu@tokenbeat.ai>
2026-03-19 14:17:26 +0800

    Initial Commit

---
+++ NoteMaster_Ver_1/NoteMaster_Ver_1App.swift
@@ -0,0 +1,17 @@
+//
+//  NoteMaster_Ver_1App.swift
+//  NoteMaster_Ver_1
+//
+//  Created by Shaun on 2026/3/19.
+//
+
+import SwiftUI
+
+@main
+struct NoteMaster_Ver_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
